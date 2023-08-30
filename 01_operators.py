a8c462a14732d02ed74000520d6e3503d3d244bb
SherazAhmadd <ranazaryab736@gmail.com>
2023-08-30 13:51:11 +0500

    Basic Operators in python

---
+++ 01_operators.py
@@ -0,0 +1,7 @@
+print("the addition of 5+3 is:",5+3)  #addition
+print(5-3)          # substruction
+print(5*3)          # multiplication
+print(5/3)          # division in floating number
+print(5//3)         # division in integer number
+#PEMDAS rule --> parenthesis exponent multiplication division addition substruction
+print(3**2/2+6*2)
