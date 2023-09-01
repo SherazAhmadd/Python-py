0b8b9dfbc306bd3354579cfb63767a48034b9f4c
SherazAhmadd <ranazaryab736@gmail.com>
2023-09-01 12:36:37 +0500

    Declaration and defining the variables in Python

---
+++ 03_variables.py
@@ -0,0 +1,29 @@
+# variable declaration 
+# varible_name assignment operator value/string or character
+x = 5
+print(x)
+x = 6.5
+print(x)
+x = "hellow babes"
+print(x)
+# how to find the type of variable
+type(x)
+print(type(x))
+print("---------------------")
+fruit_type = "mangoes\noranges\nbanana"
+print(fruit_type)
+# \n work as endl
+roll_no = 526434
+print(roll_no)
+#  del keyword is delete the keyword
+# del roll_no
+print(roll_no)
+print("------------------------------")
+# taking input from user in variables
+id_no=input("what is your id: \n")
+greeting=input("enter your message: \n")
+print("printing the output here")
+print("-------------------------")
+print(roll_no)
+print(greeting)
+print(id_no)
